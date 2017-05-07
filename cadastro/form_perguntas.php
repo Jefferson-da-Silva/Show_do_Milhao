f36667c31ffec7baa8253669a31a62b1d21764a8
randler <randlersi@gmail.com>
2017-05-07 18:41:45 -0300

    cadastro

---
+++ cadastro/form_perguntas.php
@@ -0,0 +1,152 @@
+
+
+
+
+<!-- Smart Wizard -->
+<p>This is a basic form wizard example that inherits the colors from the selected scheme.</p>
+<div id="wizard" class="form_wizard wizard_horizontal">
+    <ul class="wizard_steps">
+        <li>
+            <a href="#step-1">
+                <span class="step_no">1</span>
+                            <span class="step_descr">
+                                              Step 1<br />
+                                              <small>Step 1 description</small>
+                                          </span>
+            </a>
+        </li>
+        <li>
+            <a href="#step-2">
+                <span class="step_no">2</span>
+                            <span class="step_descr">
+                                              Step 2<br />
+                                              <small>Step 2 description</small>
+                                          </span>
+            </a>
+        </li>
+        <li>
+            <a href="#step-3">
+                <span class="step_no">3</span>
+                            <span class="step_descr">
+                                              Step 3<br />
+                                              <small>Step 3 description</small>
+                                          </span>
+            </a>
+        </li>
+        <li>
+            <a href="#step-4">
+                <span class="step_no">4</span>
+                            <span class="step_descr">
+                                              Step 4<br />
+                                              <small>Step 4 description</small>
+                                          </span>
+            </a>
+        </li>
+    </ul>
+    <div id="step-1">
+        <form class="form-horizontal" role="form">
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="inputEmail3" class="control-label">Pergunta</label>
+                </div>
+                <div class="col-sm-10">
+                    <textarea class="form-control" id="inputEmail3" placeholder="Pergunta" rows="5"></textarea>
+                </div>
+            </div>
+            <p class="lead text-center text-danger">Selecione a alternativa correta!</p>
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="inputAlternativa1" class="control-label">Alternativa 1</label>
+                    <input type="radio" name="optradio-grandeArea">
+                </div>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputAlternativa1" placeholder="Alternativa 1">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="inputAlternativa2" class="control-label">Alternativa 2</label>
+                    <input type="radio" name="optradio-grandeArea">
+                </div>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputAlternativa2" placeholder="Alternativa 2">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="inputAlternativa3" class="control-label">Alternativa 3</label>
+                    <input type="radio" name="optradio-grandeArea">
+                </div>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputAlternativa3" placeholder="Alternativa 3">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="inputAlternativa4" class="control-label">Alternativa 4</label>
+                    <input type="radio" name="optradio-grandeArea">
+                </div>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputAlternativa4" placeholder="Alternativa 4">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-2">
+                    <label for="inputAlternativa5" class="control-label">Alternativa 5</label>
+                    <input type="radio" name="optradio-grandeArea">
+                </div>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputAlternativa5" placeholder="Alternativa 5">
+                </div>
+            </div>
+        </form>
+
+    </div>
+    <div id="step-2">
+        <h2 class="StepTitle">Step 2 Content</h2>
+        <p>
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+    <div id="step-3">
+        <h2 class="StepTitle">Step 3 Content</h2>
+        <p>
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+            eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+    <div id="step-4">
+        <h2 class="StepTitle">Step 4 Content</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+
+</div>
+<!-- End SmartWizard Content -->
+
+
+
+
+
+
+
+
+
