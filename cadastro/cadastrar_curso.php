f4c556c69efae88a271289a701e14ac94a81e192
randler <randlersi@gmail.com>
2017-05-07 17:32:54 -0300

    tela ranking

--- cadastro/cadastrar_curso.php
+++ cadastro/cadastrar_curso.php
@@ -2,21 +2,20 @@
 	require_once "../conecta.php" ;
 	echo "where are you";
     $curso = $_POST['nome_do_curso'];
-    	if(isset($_POST['botao_cadastrar_curso'])){
-    		 $sql_curso = "select * from Curso where Descricao_Curso = '$curso'";
-      		$resultado_curso = mysqli_query($con, $sql_curso);
-     		 $existe_curso = mysqli_num_rows($resultado_curso);
-         if ($existe_curso) {
-           //echo "<script language='javascript' type='text/javascript'>swal('Usuario ja existente', '', 'error');</script>";
-          }
+    	if(isset($_POST['botao_cadastrar_curso'])) {
+            $sql_curso = "select * from Curso where Descricao_Curso = '$curso'";
+            $resultado_curso = mysqli_query($con, $sql_curso);
+            $existe_curso = mysqli_num_rows($resultado_curso);
+            if ($existe_curso) {
+                //echo "<script language='javascript' type='text/javascript'>swal('Usuario ja existente', '', 'error');</script>";
+            } else {
+                $sql = "insert into Curso (Descricao_Curso) value ('$curso')";
+                $insert = mysqli_query($con, $sql);
+                if ($insert) {
+                    header("Location: cadastro_jogo.html");
+                } else {
 
-          else {
-            $sql = "insert into Curso (Descricao_Curso) value ('$curso')";
-            $insert = mysqli_query($con ,$sql);
-            if ($insert) {
-                header("Location: cadastro_jogo.html");
-          }else {
-
-          }
-    	}	
+                }
+            }
+        }
 ?>
