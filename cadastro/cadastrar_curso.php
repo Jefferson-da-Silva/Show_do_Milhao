36d1af440f042c84e2037902c13b6acd5f140797
randler <randlersi@gmail.com>
2017-05-07 17:51:18 -0300

    cadastro curso

--- cadastro/cadastrar_curso.php
+++ cadastro/cadastrar_curso.php
@@ -1,19 +1,25 @@
 <?php
 	require_once "../conecta.php" ;
-	echo "where are you";
-    $curso = $_POST['nome_do_curso'];
     	if(isset($_POST['botao_cadastrar_curso'])) {
+            $curso = $_POST['nome_do_curso'];
             $sql_curso = "select * from Curso where Descricao_Curso = '$curso'";
             $resultado_curso = mysqli_query($con, $sql_curso);
             $existe_curso = mysqli_num_rows($resultado_curso);
-            if ($existe_curso) {
+            if ($existe_curso > 0) {
+
+                echo "existe > 0";
                 //echo "<script language='javascript' type='text/javascript'>swal('Usuario ja existente', '', 'error');</script>";
             } else {
+
+                echo "existe < 0";
                 $sql = "insert into Curso (Descricao_Curso) value ('$curso')";
                 $insert = mysqli_query($con, $sql);
                 if ($insert) {
                     header("Location: cadastro_jogo.html");
                 } else {
+
+                    echo mysqli_error($con);
+                    echo "não inseriu";
 
                 }
             }
